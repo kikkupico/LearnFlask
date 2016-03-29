e691141fd003aeb052c40e2b38262184113f5fbb
vramakin <vramak@gmail.com>
2016-03-29 09:55:48 +0000

    added simple api method

--- flaskr.py
+++ flaskr.py
@@ -12,7 +12,7 @@
 
 from sqlite3 import dbapi2 as sqlite3
 from flask import Flask, request, session, g, redirect, url_for, abort, \
-     render_template, flash, _app_ctx_stack
+     render_template, flash, _app_ctx_stack, jsonify
 
 # configuration
 DATABASE = '/tmp/flaskr.db'
@@ -64,6 +64,14 @@ def show_entries():
     entries = cur.fetchall()
     return render_template('show_entries.html', entries=entries)
 
+
+@app.route('/api/entries/', methods=['GET'])
+def api_get_entries():
+    db = get_db()
+    cur = db.execute('select title, text from entries order by id desc')
+    entries = cur.fetchall()
+    return jsonify(entries)
+    
 
 @app.route('/add', methods=['POST'])
 def add_entry():

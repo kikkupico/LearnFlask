5e6f7bc68ca8570466170e2b07ef93fd385e3e90
vramakin <vramak@gmail.com>
2016-03-30 12:13:54 +0000

    able to handle 301 but still fails

--- flaskr.py
+++ flaskr.py
@@ -13,6 +13,54 @@
 from sqlite3 import dbapi2 as sqlite3
 from flask import Flask, request, session, g, redirect, url_for, abort, \
      render_template, flash, _app_ctx_stack, jsonify
+from flask.ext.cors import CORS
+
+from datetime import timedelta
+from flask import make_response, request, current_app
+from functools import update_wrapper
+
+
+def crossdomain(origin=None, methods=None, headers=None,
+                max_age=1, attach_to_all=False,
+                automatic_options=True):
+    if methods is not None:
+        methods = ', '.join(sorted(x.upper() for x in methods))
+    if headers is not None and not isinstance(headers, basestring):
+        headers = ', '.join(x.upper() for x in headers)
+    if not isinstance(origin, basestring):
+        origin = ', '.join(origin)
+    if isinstance(max_age, timedelta):
+        max_age = max_age.total_seconds()
+
+    def get_methods():
+        if methods is not None:
+            return methods
+
+        options_resp = current_app.make_default_options_response()
+        return options_resp.headers['allow']
+
+    def decorator(f):
+        def wrapped_function(*args, **kwargs):
+            if automatic_options and request.method == 'OPTIONS':
+                resp = current_app.make_default_options_response()
+            else:
+                resp = make_response(f(*args, **kwargs))
+            if not attach_to_all and request.method != 'OPTIONS':
+                return resp
+
+            h = resp.headers
+
+            h['Access-Control-Allow-Origin'] = origin
+            h['Access-Control-Allow-Methods'] = get_methods()
+            h['Access-Control-Max-Age'] = str(max_age)
+            if headers is not None:
+                h['Access-Control-Allow-Headers'] = headers
+            return resp
+
+        f.provide_automatic_options = False
+        return update_wrapper(wrapped_function, f)
+    return decorator
+
 
 # configuration
 DATABASE = '/tmp/flaskr.db'
@@ -23,9 +71,9 @@ PASSWORD = 'default'
 
 # create our little application :)
 app = Flask(__name__)
+cors = CORS(app)
 app.config.from_object(__name__)
 app.config.from_envvar('FLASKR_SETTINGS', silent=True)
-
 
 def init_db():
     """Creates the database tables."""
@@ -66,12 +114,13 @@ def show_entries():
 
 
 @app.route('/api/entries/', methods=['GET'])
+#@crossdomain(origin='http://vramak.koding.io')
 def api_get_entries():
     db = get_db()
     cur = db.execute('select title, text from entries order by id desc')
     entries = cur.fetchall()
     return jsonify(entries)
-    
+   
 
 @app.route('/add', methods=['POST'])
 def add_entry():
